f8c71b62d24be4063ac11635e4aee2e260c23dbc
AleksMoistus <a.moistus@gmail.com>
2022-03-08 00:38:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const hike1 = {
+  id: 0,
+  name: "Forest hike",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
+  imgURL: "../../public/Assets/foresthike.jpg"
+}
+const hike2 = {
+  id: 1,
+  name: "Mountain hike",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
+  imgURL: "../../public/Assets/foresthike.jpg"
+}
+const hike3 = {
+  id: 2,
+  name: "Cross terrain hike",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
+  imgURL: "../../public/Assets/foresthike.jpg"
+}
+
+const matkad = [matk1, matk2, matk3]
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))

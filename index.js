80f1cdba8ac96cb616c725eb02731097910551a4
AleksMoistus <a.moistus@gmail.com>
2022-03-12 22:32:37 +0200

    ll

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ const hike1 = {
   id: 0,
   name: "Forest hike",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
-  imgURL: "../../public/Assets/foresthike.jpg"
+  imgURL: "../../public/Assets/foresthike.jpg",
+  part: ['mati@matkaja.ee', 'kati@matkaja.ee'],
 }
 const hike2 = {
   id: 1,
   name: "Mountain hike",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
-  imgURL: "../../public/Assets/foresthike.jpg"
+  imgURL: "../../public/Assets/foresthike.jpg",
+  part: ['klaabu@gmail.com'],
 }
 const hike3 = {
   id: 2,
   name: "Cross terrain hike",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
-  imgURL: "../../public/Assets/foresthike.jpg"
+  imgURL: "../../public/Assets/foresthike.jpg",
+  part: [],
 }
 
-const hikes = [hike1, hike2, hike3]
+const hikes = [
+  hike1, 
+  hike2, 
+  hike3,
+{
+  id: 3,
+  name: "Beach hike",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laboriosam dolor ex, incidunt facere similique beatae totam ratione, culpa, iusto et aperiam nostrum tenetur hic minima itaque perspiciatis rerum distinctio.",
+  imgURL: "../../public/Assets/foresthike.jpg",
+  part: [],
+}
 
+]
+
+function showRegister(req, res){
+  const index = parseInt(req.params.hike)
+  console.log('valitud matk' + index)
+  res.render('pages/register', {hike: hikes[index]})
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -31,5 +51,6 @@ express()
   .get('/', (req, res) => res.render('pages/index', {hikes: hikes}))
   .get('/kontakt', (req, res) => res.render('pages/kontakt'))
   .get('/upcoming', (req, res) => res.render('pages/upcoming'))
+  .get('/register/:hike', showRegister)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
